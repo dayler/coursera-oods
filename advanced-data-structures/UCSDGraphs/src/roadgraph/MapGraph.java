e218a294b5dddb86407ec40da3ec8ab505460f95
Ariel Salazar <arielsalazar@asalazar-4951.nuevatel.com>
2017-11-20 21:06:20 -0400

    Add quiz response.

--- advanced-data-structures/UCSDGraphs/src/roadgraph/MapGraph.java
+++ advanced-data-structures/UCSDGraphs/src/roadgraph/MapGraph.java
@@ -272,6 +272,7 @@ public class MapGraph {
             // hook visualization.
             nodeSearched.accept(current);
         }
+        System.out.println("settled:" + settled.size());
         // build and get path.
         return parentMap.isEmpty() ? Collections.emptyList() : buildPath(parentMap, start, goal);
     }
@@ -304,7 +305,7 @@ public class MapGraph {
         return findPath(start, goal, nodeSearched, (gp, gpGoal) -> gp.distance(gpGoal));
     }
     
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         System.out.print("Making a new map...");
         MapGraph firstMap = new MapGraph();
         System.out.print("DONE. \nLoading the map...");
@@ -374,4 +375,50 @@ public class MapGraph {
         System.out.println("DONE.");
     }
 
+    public static void main3(String[] args) {
+        MapGraph simpleTestMap = new MapGraph();
+        GraphLoader.loadRoadMap("data/testdata/simpletest.map", simpleTestMap);
+
+        GeographicPoint testStart = new GeographicPoint(1.0, 1.0);
+        GeographicPoint testEnd = new GeographicPoint(8.0, -1.0);
+
+        System.out.println("Test 1 using simpletest: Dijkstra should be 9 and AStar should be 5");
+        List<GeographicPoint> testroute = simpleTestMap.dijkstra(testStart, testEnd);
+        List<GeographicPoint> testroute2 = simpleTestMap.aStarSearch(testStart, testEnd);
+
+        MapGraph testMap = new MapGraph();
+        GraphLoader.loadRoadMap("data/maps/utc.map", testMap);
+
+        // A very simple test using real data
+        testStart = new GeographicPoint(32.869423, -117.220917);
+        testEnd = new GeographicPoint(32.869255, -117.216927);
+        System.out.println("Test 2 using utc: Dijkstra should be 13 and AStar should be 5");
+        testroute = testMap.dijkstra(testStart, testEnd);
+        testroute2 = testMap.aStarSearch(testStart, testEnd);
+
+        // A slightly more complex test using real data
+        testStart = new GeographicPoint(32.8674388, -117.2190213);
+        testEnd = new GeographicPoint(32.8697828, -117.2244506);
+        System.out.println("Test 3 using utc: Dijkstra should be 37 and AStar should be 10");
+        testroute = testMap.dijkstra(testStart, testEnd);
+        testroute2 = testMap.aStarSearch(testStart, testEnd);
+    }
+    
+    public static void main(String[] args) {
+        /* Use this code in Week 3 End of Week Quiz */
+
+        MapGraph theMap = new MapGraph();
+        System.out.print("DONE. \nLoading the map...");
+        GraphLoader.loadRoadMap("data/maps/utc.map", theMap);
+        System.out.println("DONE.");
+
+        GeographicPoint start = new GeographicPoint(32.8648772, -117.2254046);
+        GeographicPoint end = new GeographicPoint(32.8660691, -117.217393);
+
+        List<GeographicPoint> route = theMap.dijkstra(start, end);
+        List<GeographicPoint> route2 = theMap.aStarSearch(start, end);
+
+        System.out.println("Dijkstra: " + route.size() + ", AStarSearch: " + route2.size());
+        System.out.println("DONE.");
+    }
 }
